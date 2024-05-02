6eb387d88d4855b6f777528731ae0b766c07cc95
Paulo Victor Lima <pvldotz@gmail.com>
2024-05-02 11:15:25 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -15,7 +15,7 @@ app.register(getUser)
 app.register(getAllUsers)
 
 app.listen({
-    /* host: '0.0.0.0', */
+    host: '0.0.0.0',
     port: 3333,
 }).then(() => {
     console.log(`HTTP Server running on port ${port}!!!`);
